6963e8d35839349c050d0d4972e9f7269761a46e
Standvpmnt <shubhang.saltech@gmail.com>
2019-08-24 02:42:30 +0530

    Fixes for create, flash messages and other edits

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,9 +4,18 @@ class ArticlesController < ApplicationController
 	end
 	def create
 		@article = Article.new(article_params)
-		@article.save
-		redirect_to_articles_show(@article)
+		#@article.save
+		if @article.save
+			flash[:notice] = "Article was successfully created"
+			redirect_to article_path(@article)
+		else
+			render 'new'
+		end
+		#redirect_to_article_path(@article)
 		#render plain: params[:article].inspect
+	end
+	def show
+		@article = Article.find(params[:id])
 	end
 
 	private
