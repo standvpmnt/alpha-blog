50d0b9ed4151eaa04f1699d912b1a5de0099d188
Standvpmnt <shubhang.saltech@gmail.com>
2019-08-26 00:13:17 +0530

    changes made to include flash message for successful article creation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,7 +9,7 @@ class ArticlesController < ApplicationController
 			flash[:notice] = "Article was successfully created"
 			redirect_to article_path(@article)
 		else
-			render 'new'
+			render :new
 		end
 		#redirect_to_article_path(@article)
 		#render plain: params[:article].inspect
