02b39b86f511286a0faa35d8afe6655145745f83
Standvpmnt <shubhang.saltech@gmail.com>
2019-09-07 02:53:25 +0530

    Add admin requirement to create categories

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,6 +3,8 @@ require 'test_helper'
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
 	def setup
 		@category = Category.create(name: "sports")
+		@user = User.create(username: "John", email: "john@example.com", password: "password", admin: true)
+
 	end
 
 
@@ -12,6 +14,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
 	end
 
 	test "should get new" do
+		sign_in_as(@user, "password")
 		get new_category_path
 		assert_response :success
 	end
@@ -19,5 +22,12 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
 	test "should get show" do
 		get category_path(@category)
 		assert_response :success
+	end
+
+	test "should redirect create when admin not logged in" do 
+		assert_no_difference 'Category.count' do
+			post categories_path, params: {category:{name: "sports"}}
+		end
+		assert_redirected_to categories_path
 	end
 end
